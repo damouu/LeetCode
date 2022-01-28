ac790ed6136370a3016e2aeb04f1f8eefe6e80cd
damou_ <mouadsehbaoui@gmail.com>
2022-01-28 16:41:29 +0100

    Challenge Done
     #LC-1 Done Unassigned Fixed Done

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+def duplicateZeros(array):
+    i = 0
+    while i < len(array):
+        if array[i] == 0:
+            positionChange = array[i + 1:-1]
+            array[i + 1] = 0
+            del array[i + 2:]
+            array = array + positionChange
+            positionChange.clear()
+            i += 2
+        else:
+            i += 1
+    return array
+
+
 class Solution(object):
-    print('dede')
+    array = duplicateZeros([1, 0, 2, 3, 0, 4, 5, 0])
+    print(array)
