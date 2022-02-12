653ed11e706eefb7e3b2098194fcba8da665fee2
damou_ <mouadsehbaoui@gmail.com>
2022-02-12 13:04:38 +0100

    #LC-3 Done Unassigned Resolved Done

--- main.py
+++ main.py
@@ -1,54 +1,23 @@
-def merge(nums1, m, nums2, n):
-    if m != len(nums1):
-        while len(nums1) != m:
-            del nums1[-1]
-    elif n != len(nums2):
-        while len(nums2) != n:
-            del nums2[-1]
-
-    if not nums1 or not nums2:
-        if not nums1:
-            for key, value in enumerate(nums2):
-                nums1.append(nums2[key])
-
-    elif nums1[0] > nums2[0]:
-        dede = nums1[:m]
-        while len(nums1) != 0:
-            for key, value in enumerate(nums1):
-                del nums1[key]
-        while m + n != len(nums1):
-            for key, value in enumerate(nums2):
-                nums1.append(nums2[key])
-            for key, value in enumerate(dede):
-                nums1.append(dede[key])
-        for key, value in enumerate(nums1[:-1]):
-            if nums1[key] > nums1[key + 1]:
-                x = nums1[key]
-                nums1[key] = nums1[key + 1]
-                nums1[key + 1] = x
-            for key, value in enumerate(nums1[:-1]):
-                if nums1[key] > nums1[key + 1]:
-                    x = nums1[key]
-                    nums1[key] = nums1[key + 1]
-                    nums1[key + 1] = x
-    else:
-        while m + n != len(nums1):
-            for key, value in enumerate(nums2):
-                nums1.append(nums2[key])
-        for key, value in enumerate(nums1[:-1]):
-            if nums1[key] > nums1[key + 1]:
-                x = nums1[key]
-                nums1[key] = nums1[key + 1]
-                nums1[key + 1] = x
-            for key, value in enumerate(nums1[:-1]):
-                if nums1[key] > nums1[key + 1]:
-                    x = nums1[key]
-                    nums1[key] = nums1[key + 1]
-                    nums1[key + 1] = x
+def duplicateZeros(array):
+    i = 0
+    lenn = len(array)
+    while i < len(array):
+        if i == (len(array) - 1) and array[-1] == 0:
+            break
+        if array[i] == 0 and array[i + 1] != 0:
+            positionChange = array[i + 1:-1]
+            array[i + 1] = 0
+            del array[i + 2:]
+            for key, value in enumerate(positionChange[:lenn - i]):
+                array.append(value)
+            for key, value in enumerate(positionChange):
+                positionChange[key] = 0
+            i += 2
+        else:
+            i += 1
+    return array
 
 
 class Solution(object):
-    nums1 = [1, 2, 3, 0, 0, 0]
-    nums2 = [2, 5, 6]
-    merge(nums1, 3, nums2, 3)
-    print(nums1)
+    array = duplicateZeros([0, 0, 0, 0, 0, 0, 0])
+    print(array)
